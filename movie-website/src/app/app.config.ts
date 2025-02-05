1d8a19750042663cd58bd03f901b1c264f78ffca
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2025-02-05 22:15:32 +0200

    adding filltering and search

--- movie-website/src/app/app.config.ts
+++ movie-website/src/app/app.config.ts
@@ -1,10 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';  // Import HttpClientModule
+import { MovieService } from './services/movie.service'; // Import MovieService
+import { FormsModule, NgModel } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    FormsModule,
+    NgModel
+  ]
 };
--- movie-website/src/app/movie-list/movie-list.component.ts
+++ movie-website/src/app/movie-list/movie-list.component.ts
@@ -4,22 +4,57 @@ import { AsyncPipe, NgFor } from '@angular/common';
 import { MovieItemComponent } from '../movie-Item/movie-Item.component';
 import { MovieItem } from '../../Models/MovieItem';
 import { MovieService } from '../services/movie.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css'],
   standalone:true,
-  imports: [AsyncPipe,MovieItemComponent,NgFor]
+  imports: [AsyncPipe,MovieItemComponent,NgFor,FormsModule ]
 })
 export class MovieListComponent implements OnInit {
-  moviesList$!:Observable<MovieItem[]>
+  moviesList$!: Observable<MovieItem[]>;
+  filteredMovies: MovieItem[] = [];
+  searchQuery: string = '';
+  sortOption: string = 'title';
   constructor(private movieService:MovieService) { }
 
   ngOnInit() {
     this.moviesList$ = this.movieService.getAllMovies();
-
-
+    this.moviesList$.subscribe((movies) => {
+      this.filteredMovies = movies;
+    });
   }
+  onSearch(): void {
+    let searchResults = this.filteredMovies;
 
+    if (this.searchQuery.trim()) {
+      searchResults = searchResults.filter((movie) =>
+        movie.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+
+    }
+    else {
+      this.moviesList$.subscribe((movies) => {
+        this.filteredMovies = movies;
+      });
+    }
+
+    this.sortMovies(searchResults);
+  }
+  onSort(): void {
+    this.sortMovies(this.filteredMovies);
+  }
+  sortMovies(movies: MovieItem[]): void {
+    if (this.sortOption === 'title') {
+      this.filteredMovies = movies.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+    } else if (this.sortOption === 'year') {
+      this.filteredMovies = movies.sort((a, b) =>
+        parseInt(a.year) - parseInt(b.year)
+      );
+    }
+  }
 }
